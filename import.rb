b278479d624023b05081a95e8dd004ce52092da2
Jason Holder <jason@notacc.net>
2016-04-04 12:37:58 -0500

    Update import.rb

--- import.rb
+++ import.rb
@@ -6,7 +6,7 @@ require_relative 'invoice'
 module Imports
 
   def import_invoices(invoice_file,collection_name)
-    #id,date,client_id,uri
+    #id,date,client_id,uri,ref
     CSV.foreach(invoice_file, headers: true, header_converters: :symbol, skip_blanks: true, converters: :all) do |row|
       invoice = Invoice.new(row[:id],row[:date],row[:client_id],row[:uri],row[:ref])
       collection_name.add_invoice(invoice)
