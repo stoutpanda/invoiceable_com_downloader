d7ac5973f2554a116e7b0461313feb6e7a2e92fa
StoutPanda <jason@notacc.net>
2016-04-03 23:20:30 -0500

    Added progress bar and download_all_invoices method.

--- invoice.rb
+++ invoice.rb
@@ -1,9 +1,11 @@
 require 'date'
 require 'open-uri'
+require 'progressbar'
 
 
 class Invoice
   attr_reader :id, :date, :client_id, :uri, :uri_key, :ref
+
 
   def initialize(id,date,client_id,uri,ref)
     @id = id
@@ -20,11 +22,20 @@ class Invoice
 
   def download_invoice(output_dir)
     download_url = URI.parse("https://secure.invoiceable.com/downloadinvoice/#{@uri_key}")
+    pbar = nil
     File.open("#{output_dir}#{@ref}-#{@date.to_s}.pdf", "wb") do |file|
-      file.write open(download_url.to_s).read
-    end
+      file.write open(download_url.to_s,
+        :content_length_proc => lambda { |t|
+        if t && 0 < t
+          pbar = ProgressBar.new("...", t)
+          pbar.file_transfer_mode
+        end
+        },:progress_proc => lambda {|s|
+          pbar.set s if pbar
+            }).read
+          end
+      end
   end
-end
 
 class Invoices
 
@@ -37,5 +48,10 @@ class Invoices
   def add_invoice(invoice)
     @invoices << invoice
   end
+
+  def download_all_invoices(output_dir)
+    @invoices.each { |invoice| invoice.download_invoice(output_dir) }
+  end
+
 
 end
