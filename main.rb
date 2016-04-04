092e0dad881bc814945a1ffd0cc48b03643ed60e
StoutPanda <jason@notacc.net>
2016-04-03 23:39:13 -0500

    Adding Licences.txt

--- main.rb
+++ main.rb
@@ -10,16 +10,3 @@ collection = Invoices.new
 import_invoices(invoice_file,collection)
 
 collection.download_all_invoices(output_dir)
-
-
- #create a new object for each with accessbile infomration above.
-
- #iterate through each hash in the array, and format the date.
-
- #iterate through each hash in the array, and make a new key in the hash of url_key which will be used for later.
-
- #define pdf saved name for each file
-
- #parse user creds into new object for user
-
- #login to website as user, then visit download url for each url_key, save the file in the output directory and then
